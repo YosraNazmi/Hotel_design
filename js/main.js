895a1d7b1b0f7a715b6735b4bddfbeb4738e841a
YosraNazmi <39218896+YosraNazmi@users.noreply.github.com>
2020-04-30 10:27:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,82 @@
+//navbar dropdown
+jQuery(document).ready(function($) {
+	//call dropdown hover and on load & resize
+	$( window ).resize(function() {
+		enableHover();
+	});
+	enableHover();
+});
+
+//emable hover when window > 767px 
+function enableHover() {
+    if ($(window).width() > 767) {
+        $('nav.navbar li.dropdown').hover(function() {
+              $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+            }, function() {
+              $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+        });
+    } else {
+        $('nav.navbar li.dropdown').unbind('mouseenter mouseleave')
+    }
+}
+
+//date picker
+
+$(".datepicker-1").datepicker();
+$(".datepicker-2").datepicker();   
+
+//selection of rooms
+$(document).ready(function() {
+  $('select').niceSelect();
+});
+
+
+//increasing and decreasing the room num and guest num
+ var proQty = $('.pro-qty');
+    proQty.prepend('<span class="dec qtybtn">-</span>');
+    proQty.append('<span class="inc qtybtn">+</span>');
+    proQty.on('click', '.qtybtn', function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        $button.parent().find('input').val(newVal);
+    });
+
+    
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+   
